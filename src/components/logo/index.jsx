1618b85de5548d2e72f6bbfe6264e4ec142bc544
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-05-07 19:23:59 +0100

    header - setting up of logo component;

---
+++ src/components/logo/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './logo.style.scss';
+
+const Logo = () => {
+    return (
+        <h1 className="logo-brand">LanKaDeV</h1>
+    )
+}
+export default Logo;
--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import './nav.style.scss';
+import Logo from '../logo';
 
 const Nav = () => {
     return (
             <nav className="nav" >
-                <ul className="nav-section">
-                    <li className="nav-item"><a href="/">About Me</a></li>
-                    <li className="nav-item"><a href="/skill">Skill</a></li>
-                    <li className="nav-item"><a href="/projects">Projects</a></li>
-                    <li className="nav-item"><a href="/experience">Experience</a></li>
-                    <li className="nav-item"><a href="/education">Education</a></li>
-                    <li className="nav-item"><a href="/contact">Contact</a></li>
-                </ul>
+                <div>
+                   <Logo /> 
+                </div>
+                <div>
+                    <ul className="nav-section">
+                        <li className="nav-item"><a href="/">About Me</a></li>
+                        <li className="nav-item"><a href="/skill">Skill</a></li>
+                        <li className="nav-item"><a href="/projects">Projects</a></li>
+                        <li className="nav-item"><a href="/experience">Experience</a></li>
+                        <li className="nav-item"><a href="/education">Education</a></li>
+                        <li className="nav-item"><a href="/contact">Contact</a></li>
+                    </ul>
+                </div>
             </nav>
     )
 }
