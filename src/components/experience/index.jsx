130cd711e33d63e6c6df7f87602ecc541a6fef79
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-05-11 19:04:34 +0100

    completion of experience component - laying out of contact component'

--- src/components/experience/index.jsx
+++ src/components/experience/index.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
 import './experience.style.scss';
-import { AlcImg } from '../../images/experience';
+import Experiences from '../../store/experience';
 
 const Experience = () => {
     return (
         <section className="experiences">
             <div className = "experiences-title">Experience</div>
             <div className="experiences-section">
-                <div className="experience">
-                    <div className="experience-header">
-                        <span className="company-info">
-                            <img src={AlcImg} alt="company Logo"/>
-                            <h3>AlcWithGoogle</h3>
-                        </span>
-                        <h3 className="job-role">Frontend Developer Intern</h3>
-                    </div>
-                    <div className="experience-section">
-                        <p className="experience-description">job entails - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, sed.</p>
-                        <div className="experience-accomplishment">
-                            accomplishments - Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, est?
-                        </div>
-                        <div className="experience-date">March, 2019 - February, 2020</div>
-                    </div>
-                </div>
+
+                {
+                    Experiences.map(experience => {
+                        return (
+                            <div className="experience" key={experience.id}>
+                                <div className="experience-header">
+                                    <span className="company-info">
+                                        <img src={experience.logoUrl} alt="company Logo"/>
+                                        <h3>{experience.companyName}</h3>
+                                    </span>
+                                    <h3 className="job-role">{experience.jobRole}</h3>
+                                </div>
+                                <div className="experience-section">
+                                    <p className="experience-description">{experience.companyProduct}</p>
+                                    <div className="experience-accomplishment">
+                                        {/* <h3 className="accomplishment-title">accomplishments</h3> */}
+                                        <ul>
+                                            {
+                                                experience.accomplishments.map((accomplishment, index) => {
+                                                    return(
+                                                        <li key={index}>{accomplishment}</li>
+                                                    )
+                                                })
+                                            }
+                                        </ul>
+                                    </div>
+                                    <div className="experience-date">{experience.date}</div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </section>
     )
---
+++ src/store/experience.js
@@ -0,0 +1,30 @@
+import { AlcImg } from "../images/experience";
+
+const Experiences  = [
+    {
+        "id": 2,
+        "logoUrl": AlcImg,
+        "companyName" : "AlcWithGoogle",
+        "jobRole": "Volunteer JavaScript Mentor",
+        "companyProduct": "An online practice-based learning platform open to everyone interested in acquiring technical skills, regardless of their educational background.",
+        "accomplishments": [
+            "Volunteer mentoring of fellow participants in the batch B category on the use of Javascript.",
+        ],
+        "date": "August, 2019 - December, 2020"
+    },
+    {
+        "id": 1,
+        "logoUrl": AlcImg,
+        "companyName" : "AlcWithGoogle",
+        "jobRole": "Frontend Developer Intern",
+        "companyProduct": "An online practice-based learning platform open to everyone interested in acquiring technical skills, regardless of their educational background.",
+        "accomplishments": [
+            "Practice - consolidating on my knowledge of HTML, CSS, and JavaScript.",
+            "Creation of Projects for every topic in the mobile web specialist path."
+        ],
+        "date": "April, 2019 - February, 2020"
+    },
+
+]
+
+export default Experiences;
