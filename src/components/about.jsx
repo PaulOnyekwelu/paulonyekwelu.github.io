e19688ee1bf3855d6c5452f20a7d3299a72d3b29
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-05-29 16:46:24 +0100

    code refactoring - editing of aboutme section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,18 +7,19 @@ const AboutMe = () => {
             <SectionHeader title="ABOUT ME" />
             <div className = "about-me-text">
                 <p>
-                    My name is Paul Onyekwelu and I am a software Developer always in beta mode. 
+                    Hi, my name is Paul Onyekwelu and I am a full-Stack software Developer always in beta mode. 
                     I am an Autodidact when it comes to software development. I have  
-                    great passion for creating and innovating using available technologies.
+                    great passion for creating and innovating using available technologies and huge interest in
+                    blockchain and cyber-security.
                     Although my Career path has taken many twists and turns — from 
                     hospital Pharmacy to community practice and now software Development — I've 
                     never stopped engaging my passion when it comes to problem solving.
                 </p>
-                <p>
+                {/* <p>
                     As a software developer, I enjoy using my obsessive attention to detail, my unequivocal 
                     love for making things, and my mission-driven work ethic to literally change the world. 
                     That's why I’m excited to make a big impact at a high growth company.
-                </p>
+                </p> */}
             </div>
         </section>
     )
